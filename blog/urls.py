665d503002e8384facbe61299de88e6a24ead3ca
kuboyoshi7 <kuboyoshi7@gmail.com>
2023-03-07 13:32:16 +0900

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,12 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
+
+# <int:pk> – この部分はトリッキーです。これはDjangoは整数の値を期待し、その値がpkという名前の変数で
+# ビューに渡されることを意味しています。
+# / – それからURLの最後に再び / が必要です。
+# つまり、ブラウザにhttp://127.0.0.1:8000/post/5/を入力すると、Djangoはpost_detailというビューを
+# 探していると理解します。そしてpkが5という情報をそのビューに転送します
+# https://tutorial.djangogirls.org/ja/extend_your_application/
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 
@@ -7,4 +7,8 @@ def post_list(request): # request:ネットでユーザから受け取った全�
     #posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     posts = Post.objects.all()
     return render(request, 'blog/post_list.html', {'posts':posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post':post})
 
